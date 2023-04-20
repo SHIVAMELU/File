a34aec0a3a3c09234ee0bea9d564020ca74e233c
Kanekixwof <117969841+Kanekixwof@users.noreply.github.com>
2023-04-20 23:22:33 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -6,25 +6,22 @@ from pyrogram.types import InlineKeyboardButton
 
 class Data:
     HELP = """
-<b> ❏ Perintah untuk Pengguna BOT
- ├ /start - Mulai Bot
- ├ /about - Tentang Bot ini
- ├ /help - Bantuan Perintah Bot ini
- ├ /ping - Untuk mengecek bot hidup
- └ /uptime - Untuk melihat status bot 
+<b> ❏ Commands for BOT Users
+ ├ /start - Start Bot
+ ├ /about - About this Bot
+ ├ /help - Help Command this Bot
+ ├ /ping - To check live bot
+ └ /uptime - To see bot status
  
- ❏ Perintah Untuk Admin BOT
- ├ /logs - Untuk melihat logs bot
- ├ /setvar - Untuk mengatur var dengan command dibot
- ├ /delvar - Untuk menghapus var dengan command dibot
- ├ /getvar - Untuk melihat salah satu var dengan command dibot
- ├ /users - Untuk melihat statistik pengguna bot
- ├ /batch - Untuk membuat link lebih dari satu file
- ├ /speedtest - Untuk Mengetes kecepatan server bot
- └ /broadcast - Untuk mengirim pesan broadcast ke pengguna bot
-
-👨‍💻 Develoved by </b><a href='https://t.me/Lunatic0de/101'>@Lunatic0de</a>
-"""
+ ❏ Command For Admin BOT
+ ├ /logs - To view bot logs
+ ├ /setvar - To set var with dibot command
+ ├ /delvar - To remove var with bot command
+ ├ /getvar - To see one of the vars with the bot command
+ ├ /users - To view bot user statistics
+ ├ /batch - To link more than one file
+ ├ /speedtest - To Test the bot server speed
+ └ /broadcast - To send broadcast messages to bot users
 
     close = [
         [InlineKeyboardButton("ᴛᴜᴛᴜᴘ", callback_data="close")]
@@ -45,13 +42,8 @@ class Data:
     ]
 
     ABOUT = """
-<b>Tentang Bot ini:
+<b>About this Bot:
 
-@{} adalah Bot Telegram untuk menyimpan Postingan atau File yang dapat Diakses melalui Link Khusus.
+@{} is a Telegram Bot for storing posts or files that can be accessed via a special link..
 
  • Creator: @{}
- • Framework: <a href='https://docs.pyrogram.org'>Pyrogram</a>
- • Source Code: <a href='https://github.com/mrismanaziz/File-Sharing-Man'>File-Sharing-Man v4</a>
-
-👨‍💻 Develoved by </b><a href='https://t.me/Lunatic0de/101'>@Lunatic0de</a>
-"""
